a2f060236df0b687736b1895d7f70adc95c51785
Aigars <aigarspeda@gmail.com>
2023-02-13 10:49:57 +0200

    added Switch component

--- src/components/containers/NewGameContainer/NewGameContainer.tsx
+++ src/components/containers/NewGameContainer/NewGameContainer.tsx
@@ -1,11 +1,13 @@
+import Button from "components/elements/Button/Button";
+import Input from "components/elements/Input/Input";
 import ModalWrap from "components/elements/Modal/Modal";
 import RoundButton from "components/elements/RoundButton/RoundButton";
+import Switch from "components/elements/Switch/Switch";
 import type { FC } from "react";
 import { useState } from "react";
-import Button from "../../elements/Button/Button";
-import Input from "../../elements/Input/Input";
 
 const NewGameContainer: FC = () => {
+  const [isTeams, setIsTeams] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [tournamentName, setTournamentName] = useState("");
 
@@ -28,19 +30,31 @@ const NewGameContainer: FC = () => {
         }}
       />
       <ModalWrap
-        modalTitle="Crete new tournament"
+        modalWidth="4xl"
         isModalVisible={isModalOpen}
+        modalTitle="Crete new tournament"
         handleCancelClick={() => {
           setIsModalOpen(false);
         }}
       >
-        <Input
-          name="tournamentName"
-          label="Name of tournament"
-          handleInputChange={(e) => {
-            setTournamentName(e.target.value);
-          }}
-        />
+        <div className="max-w-xs">
+          <Input
+            name="tournamentName"
+            label="Name of tournament"
+            handleInputChange={(e) => {
+              setTournamentName(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <Switch
+            isOn={isTeams}
+            handleToggle={() => {
+              setIsTeams((state) => !state);
+            }}
+          />
+        </div>
+
         <div className="flex w-full justify-between">
           <Button
             btnColor="red"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,20 @@ module.exports = {
         primary: ["var(--font-roboto)"],
         secondary: ["var(--font-cabin)"],
       },
+      keyframes: {
+        slideRight: {
+          "0%": { right: "1.75rem" },
+          "100%": { right: "0.25rem" },
+        },
+        slideLeft: {
+          "0%": { left: "1.75rem" },
+          "100%": { left: "0.25rem" },
+        },
+      },
+      animation: {
+        "slide-right": "slideRight ease 0.15s",
+        "slide-left": "slideLeft ease 0.15s",
+      },
     },
   },
   plugins: [],
