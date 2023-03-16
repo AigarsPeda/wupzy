06b775f18015b32420b5425f62433f809642695d
Aigars <aigarspeda@gmail.com>
2023-03-16 11:01:17 +0200

    refactoring

--- src/utils/createParticipantMap.ts
+++ src/utils/createParticipantMap.ts
@@ -14,6 +14,11 @@ const createParticipantMap = (teams: ParticipantType[]) => {
     if (g) {
       g.push(team);
     }
+
+    g?.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+      // return a.name - b.name;
+    });
   }
 
   // sort map keys alphabetically
