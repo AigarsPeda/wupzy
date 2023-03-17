75c73f0cc245b599b3b8618e701026a5df5c98e4
Aigars <aigarspeda@gmail.com>
2023-03-17 22:25:55 +0200

    added logo

--- src/components/elements/Logo/Logo.tsx
+++ src/components/elements/Logo/Logo.tsx
@@ -3,12 +3,13 @@ import useWindowSize from "hooks/useWindowSize";
 import Link from "next/link";
 import type { FC } from "react";
 
-import Image from "next/image";
-
 const Logo: FC = () => {
   const { windowSize } = useWindowSize();
   return (
-    <Link href="/" className="bg-black font-koulen text-4xl text-gray-900">
+    <Link
+      href="/"
+      className="overflow-hidden rounded-md bg-black font-koulen text-4xl text-gray-900"
+    >
       {windowSize.width && windowSize.width <= 400 ? (
         <LogoMark className="h-auto w-14" />
       ) : (
