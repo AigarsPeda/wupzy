b49c017e89976f283a81e1fb455a1413d09ad154
Aigars <aigarspeda@gmail.com>
2023-04-28 19:16:41 +0300

    added meta tags

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,17 +3,17 @@ module.exports = {
   siteUrl,
   exclude: ["/404"],
   generateRobotsTxt: true,
-  // robotsTxtOptions: {
-  //   policies: [
-  //     {
-  //       userAgent: "*",
-  //       disallow: ["/404"],
-  //     },
-  //     { userAgent: "*", allow: "/" },
-  //   ],
-  //   additionalSitemaps: [
-  //     `${siteUrl}sitemap.xml`,
-  //     `${siteUrl}server-sitemap.xml`,
-  //   ],
-  // },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/404"],
+      },
+      { userAgent: "*", allow: "/" },
+    ],
+    additionalSitemaps: [
+      `https://${siteUrl}/sitemap.xml`,
+      // `${siteUrl}server-sitemap.xml`,
+    ],
+  },
 };
