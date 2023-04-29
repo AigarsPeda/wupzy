9838e019844cd4ab19a41e3bdf5e72315824d123
Aigars <aigarspeda@gmail.com>
2023-04-29 15:40:31 +0300

    updated font size

--- src/components/elements/CTASection/CTASection.tsx
+++ src/components/elements/CTASection/CTASection.tsx
@@ -7,7 +7,7 @@ const CTASection: FC = () => {
         <h1 className="max-w-4xl font-primary text-5xl font-extrabold md:text-7xl">
           Sign up and manage your tournaments with ease!
         </h1>
-        <p className="mx-auto mb-5 mt-10 max-w-2xl font-primary text-gray-600 md:text-lg">
+        <h2 className="mx-auto mb-5 mt-10 max-w-2xl font-primary text-gray-600 md:text-lg">
           Simplify tournament management.{" "}
           <span className="mr-1 text-gray-800">
             Our website automates the process of creating tournament tables,
@@ -15,7 +15,7 @@ const CTASection: FC = () => {
           </span>{" "}
           so you can focus on playing the game rather than managing the
           tournament.
-        </p>
+        </h2>
       </div>
     </div>
   );
