7c614fb9c302affe75735433a41d004d5cbbbf74
Aigars <aigarspeda@gmail.com>
2023-04-30 18:22:34 +0300

    added terms of service

--- src/components/elements/PageHead/PageHead.tsx
+++ src/components/elements/PageHead/PageHead.tsx
@@ -1,5 +1,6 @@
-import type { FC } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import type { FC } from "react";
 
 interface PageHeadProps {
   title: string;
@@ -12,41 +13,48 @@ const PageHead: FC<PageHeadProps> = ({
   descriptionLong,
   descriptionShort,
 }) => {
+  const router = useRouter();
+
   return (
     <Head>
       <title>{title}</title>
-      <meta name="description" content={descriptionLong} />
-      <meta property="og:description" content={descriptionShort} />
       <meta charSet="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:image" content="/logo_photo.jpg" />
+
       <meta name="robots" content="follow, index" />
-      <meta property="og:title" content={title} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:description" content={descriptionShort} />
-      <meta property="twitter:image" content="/logo_photo.jpg" />
+
+      <meta name="description" content={descriptionLong} />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta property="og:description" content={descriptionShort} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       <meta property="twitter:title" content={title} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:image" content="/logo_photo.jpg" />
+      <meta property="twitter:description" content={descriptionShort} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content="/logo_photo.jpg" />
+      <meta property="og:url" content={`www.wupzy.com${router.asPath}`} />
 
       <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/site.webmanifest" />
       <link
-        rel="apple-touch-icon"
         sizes="180x180"
+        rel="apple-touch-icon"
         href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
-        type="image/png"
         sizes="32x32"
+        type="image/png"
         href="/favicon-32x32.png"
       />
       <link
         rel="icon"
-        type="image/png"
         sizes="16x16"
+        type="image/png"
         href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 };
