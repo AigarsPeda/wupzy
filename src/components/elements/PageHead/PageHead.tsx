a4a159aa66f6898214eaf783a8dca15a10a75718
Aigars <aigarspeda@gmail.com>
2023-05-04 09:29:17 +0300

    index page update

--- src/components/elements/PageHead/PageHead.tsx
+++ src/components/elements/PageHead/PageHead.tsx
@@ -37,24 +37,28 @@ const PageHead: FC<PageHeadProps> = ({
       <meta property="og:url" content={`https://wupzy.com${router.asPath}`} />
 
       <link rel="icon" href="/favicon.ico" />
-      <link rel="manifest" href="/site.webmanifest" />
+
       <link
-        sizes="180x180"
         rel="apple-touch-icon"
+        sizes="180x180"
         href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
-        sizes="32x32"
         type="image/png"
+        sizes="32x32"
         href="/favicon-32x32.png"
       />
       <link
         rel="icon"
-        sizes="16x16"
         type="image/png"
+        sizes="16x16"
         href="/favicon-16x16.png"
       />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#111314" />
+      <meta name="msapplication-TileColor" content="#111314" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 };
