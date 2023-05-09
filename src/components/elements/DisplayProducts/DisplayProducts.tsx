8aca4eb22965c8d6d1fad5f6d398ce3d23c9026a
Aigars <aigarspeda@gmail.com>
2023-05-09 12:27:15 +0300

    free trial 5 days

--- src/components/elements/DisplayProducts/DisplayProducts.tsx
+++ src/components/elements/DisplayProducts/DisplayProducts.tsx
@@ -61,26 +61,31 @@ const DisplayProducts: FC<DisplayProductsProps> = ({
               className="absolute -inset-1 animate-slow-ping rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-30 blur transition duration-1000 group-hover:animate-none group-hover:opacity-60 group-hover:duration-200"
             ></div>
             <div className="items-top relative rounded-lg bg-white leading-none ring-1 ring-gray-900/5">
-              <div className="w-full rounded-t-lg bg-gray-50 p-4">
+              <div className="flex w-full items-center justify-between rounded-t-lg bg-gray-50 p-4">
                 <p className="text-center font-primary text-xl text-gray-700">
                   {name}
                 </p>
+                {name === "Annually" && (
+                  <p className="rounded bg-purple-500 px-2 py-1 text-sm text-white">
+                    Recommended
+                  </p>
+                )}
               </div>
+
               <div className="py-4">
-                {/* <div className="w-content px-2 py-1">
-                  <p className="text-2xl text-gray-800">{oldPrice} €</p>
-                </div> */}
-                <div className="mx-auto px-4 pt-3">
-                  <div className="mx-auto flex items-center justify-center text-center font-primary text-gray-500">
-                    <p className="text-4xl text-gray-800">
-                      {(product.unit_amount || 0) / 100}{" "}
-                      {getCurrencies(product)}
-                    </p>
-                    <p className="ml-2.5 w-10 break-normal text-left text-xs">
-                      {getPeriod(product.recurring?.interval || "month")}
-                    </p>
-                  </div>
+                <div>
+                  <p className="text-center text-gray-500">5 day free trial</p>
                 </div>
+
+                <div className="mx-auto flex items-center justify-center pt-3 text-center font-primary text-gray-500">
+                  <p className="text-4xl text-gray-800">
+                    {(product.unit_amount || 0) / 100} {getCurrencies(product)}
+                  </p>
+                  <p className="ml-3 w-10 break-normal text-left text-xs">
+                    {getPeriod(product.recurring?.interval || "month")}
+                  </p>
+                </div>
+
                 <div className="flex w-full items-center justify-center px-4 pb-4 pt-7">
                   <span className="buy-button rounded-lg bg-gray-900 px-6 py-2 text-base font-medium text-white transition-all group-hover:bg-violet-800 md:text-sm">
                     Select
--- src/components/elements/ProductsLoading/ProductsLoading.tsx
+++ src/components/elements/ProductsLoading/ProductsLoading.tsx
@@ -18,16 +18,16 @@ const PLoading: FC = () => {
       <div className="absolute -inset-1 animate-slow-ping rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-30 blur transition duration-1000 group-hover:animate-none group-hover:opacity-60 group-hover:duration-200"></div>
       <div className="items-top relative rounded-lg bg-white leading-none ring-1 ring-gray-900/5">
         <div className="w-full rounded-t-lg bg-gray-50 p-4">
-          <div className="h-10 w-full rounded-md bg-gray-200 text-center font-primary" />
+          <div className="h-11 w-full rounded-md bg-gray-200 text-center font-primary" />
         </div>
         <div className="py-4">
           <div className="mx-auto px-4 pt-3">
             <div className="mx-auto flex items-center justify-center text-center font-primary">
-              <div className="h-8 w-full rounded-md bg-gray-300" />
+              <div className="h-9 w-full rounded-md bg-gray-300" />
             </div>
           </div>
           <div className="flex w-full items-center justify-center px-4 pb-4 pt-7">
-            <div className="buy-button h-8 w-20 rounded-lg bg-gray-400  px-6 py-2 text-base font-medium text-white transition-all group-hover:bg-violet-800 md:text-sm" />
+            <div className="buy-button h-10 w-20 rounded-lg bg-gray-400  px-6 py-2 text-base font-medium text-white transition-all group-hover:bg-violet-800 md:text-sm" />
           </div>
         </div>
       </div>
