09e4e4647cca2376dba264e002e4dde5b5b862c8
Aigars <aigarspeda@gmail.com>
2023-05-10 21:59:46 +0300

    free trial

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,17 +3,5 @@ module.exports = {
   siteUrl,
   exclude: ["/404"],
   generateRobotsTxt: true,
-  // robotsTxtOptions: {
-  //   policies: [
-  //     {
-  //       userAgent: "*",
-  //       disallow: ["/404"],
-  //     },
-  //     { userAgent: "*", allow: "/" },
-  //   ],
-  //   additionalSitemaps: [
-  //     `${siteUrl}sitemap.xml`,
-  //     `${siteUrl}server-sitemap.xml`,
-  //   ],
-  // },
+  exclude: ["/signup", "/tournaments", "/login/forgot"],
 };
