352df8f1516b6990be0b27c620d03663a4aa447b
Aigars <aigarspeda@gmail.com>
2023-06-03 09:09:18 +0300

    work in progress

--- src/components/elements/Footer/Footer.tsx
+++ src/components/elements/Footer/Footer.tsx
@@ -5,11 +5,11 @@ const Footer = () => {
 
   return (
     <footer className="mt-10 h-20 w-full bg-gray-800">
-      <div className="flex h-full items-center justify-between px-10">
-        <div>
+      <div className="h-full items-center justify-between px-10 py-2 md:flex">
+        <div className="mb-2 md:mb-0">
           <p className="text-white">wupzy@wupzy.com</p>
         </div>
-        <div>
+        <div className="flex justify-between">
           <Link href="/terms-of-service" className="font-normal text-white">
             Terms of Service
           </Link>
