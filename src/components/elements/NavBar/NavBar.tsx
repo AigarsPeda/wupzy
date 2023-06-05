c2e888696f09ef92abad88472032304993bbe76c
Aigars <aigarspeda@gmail.com>
2023-06-05 13:45:18 +0300

    work in progress

--- src/components/elements/NavBar/NavBar.tsx
+++ src/components/elements/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar: FC = () => {
     <>
       <nav className="relative z-20 flex items-center justify-between bg-slate-50 px-4 py-4 shadow-[0_2px_5px_rgba(0,0,0,0.07)] md:px-12 md:py-4">
         <Logo />
-        {/* <AuthenticateUser /> */}
+        <AuthenticateUser />
       </nav>
     </>
   );
