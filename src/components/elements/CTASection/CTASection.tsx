aaf769a12e0e4d09af54b3c2208526a8b7cc6f25
Aigars <aigarspeda@gmail.com>
2023-06-05 11:03:35 +0300

    submit email

--- src/components/elements/CTASection/CTASection.tsx
+++ src/components/elements/CTASection/CTASection.tsx
@@ -19,10 +19,10 @@ const CTASection: FC = () => {
           organized, save time, and elevate your sports experience with Wupzy.
         </h2>
 
-        <p className="mx-auto my-6 max-w-2xl">
-          * Sign up now to be notified when the new and improved page is ready,
-          and as a bonus, receive 100 audition credits! Stay tuned for thrilling
-          updates and exciting features coming your way.
+        <p className="mx-auto my-6 max-w-2xl font-primary font-normal text-gray-600">
+          * Sign up now to be notified when page is ready, and as a bonus,
+          receive 100 audition credits! Stay tuned for thrilling updates and
+          exciting features coming your way.
         </p>
       </div>
     </div>
--- src/components/elements/EmailCollector/EmailCollector.tsx
+++ src/components/elements/EmailCollector/EmailCollector.tsx
@@ -9,26 +9,28 @@ const EmailCollector: FC = () => {
     api.emailListRouter.postEmail.useMutation();
 
   return (
-    <div className="flex">
+    <div className="mx-auto flex w-full justify-center">
       {isSuccess ? (
         <p className="text-center text-2xl font-black text-gray-900">
           Thanks for signing up! We&apos;ll let you know when we&apos;re ready
           to launch.
         </p>
       ) : (
-        <>
-          <div className="mr-2 w-96">
+        <div className="mx-auto flex w-full flex-col items-center justify-center md:flex-row">
+          <div className="mx-3 mb-3 w-full max-w-[24rem] md:mb-0 md:mr-2">
             <GradientInput value={email} handleInputChange={setEmail} />
           </div>
-          <GradientButton
-            btnTitle="Submit"
-            isLoading={isLoading}
-            handleClick={() => {
-              mutate({ email });
-              setEmail("");
-            }}
-          />
-        </>
+          <div className="flex items-center justify-center md:justify-start">
+            <GradientButton
+              btnTitle="Submit"
+              isLoading={isLoading}
+              handleClick={() => {
+                mutate({ email });
+                setEmail("");
+              }}
+            />
+          </div>
+        </div>
       )}
     </div>
   );
