c45e1c889651fdd9d96999537b364ff06d7aea83
Aigars <aigarspeda@gmail.com>
2023-06-16 22:09:11 +0300

    added games display

--- src/components/elements/DisplayGames/DisplayGames.tsx
+++ src/components/elements/DisplayGames/DisplayGames.tsx
@@ -5,12 +5,14 @@ import LoadingSkeleton from "~/components/elements/LoadingSkeleton/LoadingSkelet
 import NumberInput from "~/components/elements/NumberInput/NumberInput";
 import SecondaryButton from "~/components/elements/SecondaryButton/SecondaryButton";
 import TeamName from "~/components/elements/TeamName/TeamName";
+import useWindowSize from "~/hooks/useWindowSize";
 import { type GameType } from "~/types/tournament.types";
 import {
   type GamesScoresType,
   type HandleScoreChangTypeArgs,
   type HandleScoreSaveTypeArgs,
 } from "~/types/utils.types";
+import classNames from "~/utils/classNames";
 
 interface DisplayGamesProps {
   games: GameType[];
@@ -31,6 +33,25 @@ const DisplayGames: FC<DisplayGamesProps> = ({
   handleScoreSave,
   handleScoreChange,
 }) => {
+  const { windowSize } = useWindowSize();
+
+  // calculate the width of the one game card based on the window size so that the last card is cut off
+  const cardsWidth = (wind: number) => {
+    if (wind > 1100) {
+      return 25;
+    }
+
+    if (wind > 805) {
+      return 32;
+    }
+
+    if (wind > 668) {
+      return 50;
+    }
+
+    return 70;
+  };
+
   return (
     <div>
       <div className="mb-2 mt-4">
@@ -52,7 +73,12 @@ const DisplayGames: FC<DisplayGamesProps> = ({
                   key={game.id}
                   className="rounded-md border border-gray-300 bg-gradient-to-br from-gray-100 to-white p-3 shadow-sm"
                 >
-                  <div className="flex h-full w-72 flex-col justify-between">
+                  <div
+                    className={classNames(
+                      "flex h-full flex-col justify-between"
+                    )}
+                    style={{ width: `${cardsWidth(windowSize.width)}vw` }}
+                  >
                     <div className="mb-4 flex space-x-1">
                       <p className="rounded-md bg-gray-400 px-1 py-0.5 font-primary text-xs font-normal capitalize text-gray-50">
                         Game {game?.order}
@@ -61,7 +87,7 @@ const DisplayGames: FC<DisplayGamesProps> = ({
                         Round {game?.round}
                       </p>
                     </div>
-                    <div className="mb-2">
+                    <div className="mb-2 mt-4">
                       <div className="grid grid-cols-12">
                         <TeamName name={game?.teamOne?.name} />
                         <div className="col-span-2 text-center">
@@ -74,38 +100,57 @@ const DisplayGames: FC<DisplayGamesProps> = ({
                     </div>
 
                     {isWinnerFound ? (
-                      <div className="mb-3 flex h-11 justify-around">
-                        <DisplayScore
-                          score={game.teamOneSetScore}
-                          isWinner={game.teamOneId === game.winnerId}
-                        />
-                        <DisplayScore
-                          score={game.teamTwoSetScore}
-                          isWinner={game.teamTwoId === game.winnerId}
-                        />
+                      <div className="mb-4 grid grid-cols-12 ">
+                        <div className="col-span-5 text-center">
+                          <DisplayScore
+                            score={game.teamOneSetScore}
+                            isWinner={game.teamOneId === game.winnerId}
+                          />
+                        </div>
+                        <div className="col-span-2 text-center">
+                          <p className="content-center text-sm text-gray-300">
+                            {""}
+                          </p>
+                        </div>
+                        <div className="col-span-5 text-center">
+                          <DisplayScore
+                            score={game.teamTwoSetScore}
+                            isWinner={game.teamTwoId === game.winnerId}
+                          />
+                        </div>
                       </div>
                     ) : (
-                      <div className="flex justify-between pb-4">
-                        <NumberInput
-                          value={gameScore?.teamOneScore || 0}
-                          onChange={(num) => {
-                            handleScoreChange({
-                              num,
-                              gameId: game?.id,
-                              teamId: game?.teamOne?.id,
-                            });
-                          }}
-                        />
-                        <NumberInput
-                          value={gameScore?.teamTwoScore || 0}
-                          onChange={(num) => {
-                            handleScoreChange({
-                              num,
-                              gameId: game?.id,
-                              teamId: game?.teamTwo?.id,
-                            });
-                          }}
-                        />
+                      <div className="mx-auto mb-4 grid grid-cols-12">
+                        <div className="col-span-5">
+                          <NumberInput
+                            value={gameScore?.teamOneScore || 0}
+                            onChange={(num) => {
+                              handleScoreChange({
+                                num,
+                                gameId: game?.id,
+                                teamId: game?.teamOne?.id,
+                              });
+                            }}
+                          />
+                        </div>
+                        <div className="col-span-2 text-center">
+                          <p className="content-center text-sm text-gray-300">
+                            {""}
+                          </p>
+                        </div>
+
+                        <div className="col-span-5">
+                          <NumberInput
+                            value={gameScore?.teamTwoScore || 0}
+                            onChange={(num) => {
+                              handleScoreChange({
+                                num,
+                                gameId: game?.id,
+                                teamId: game?.teamTwo?.id,
+                              });
+                            }}
+                          />
+                        </div>
                       </div>
                     )}
 
