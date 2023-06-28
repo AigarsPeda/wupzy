8b285f34fe79e74b0b2e0af9535d937a27c6933c
Aigars <aigarspeda@gmail.com>
2023-06-28 06:06:18 +0300

    work in progress - split in groups

--- src/pages/new-tournament.tsx
+++ src/pages/new-tournament.tsx
@@ -1,13 +1,21 @@
 import { type NextPage } from "next";
 import NewTournamentForm from "~/components/elements/NewTournamentForm/NewTournamentForm";
 import PageHeadLine from "~/components/elements/PageHeadLine/PageHeadLine";
+import PageHead from "../components/elements/PageHead/PageHead";
 
 const NewTournamentPage: NextPage = () => {
   return (
-    <div>
+    <>
+      <PageHead
+        title="Wupzy | New tournament"
+        descriptionShort="Platform that lets you effortlessly create tournament tables."
+        descriptionLong="Wupzy is a powerful platform that lets you effortlessly create
+      tournament tables, save game scores, view real-time results, and share
+      them with all participants in just a few clicks."
+      />
       <PageHeadLine title="Create new tournament" />
       <NewTournamentForm />
-    </div>
+    </>
   );
 };
 
