8d7291baed23e8812b84349ccb47897f93f13de9
Aigars <aigarspeda@gmail.com>
2023-07-04 04:26:25 +0200

    added stripe

--- hardcoded.ts
+++ hardcoded.ts
@@ -21,4 +21,6 @@ export const ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
 export const PRICE_FOR_100_CREDITS = 345;
 
-export const STRIPE_PRICE_ID = "price_1NP7sDFTT3aSVLG6wBWIJrXN";
+export const STRIPE_PRICE_ID = "price_1NPyuIFTT3aSVLG6lVGK9JMN";
+
+export const TEST_STRIPE_PRICE_ID = "price_1NP7sDFTT3aSVLG6wBWIJrXN";
