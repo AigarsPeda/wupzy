5fac623f27394eb0fda14459313fef1230f4866d
Aigars <aigarspeda@gmail.com>
2023-07-30 07:24:57 +0300

    work in progress - adding playoffs

--- src/pages/tournaments/[id].tsx
+++ src/pages/tournaments/[id].tsx
@@ -41,6 +41,27 @@ const TournamentPage: NextPage = () => {
     "group"
   );
 
+  // const fil = (fn, a) => {
+  //   const f = []; //final
+  //   for (let i = 0; i < a.length; i++) {
+  //     if (fn(a[i])) {
+  //       f.push(a[i]);
+  //     }
+  //   }
+  //   return f;
+  // };
+
+  // fast filter function
+  const ff = (fn: (a: any) => boolean, a: any[]) => {
+    const f = []; //final
+    for (let i = 0; i < a.length; i++) {
+      if (fn(a[i])) {
+        f.push(a[i]);
+      }
+    }
+    return f;
+  };
+
   return (
     <>
       <PageHead
